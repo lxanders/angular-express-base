7a5928c10a1481f06370c3313ac35a8c101f3ebe
Alexander Schmidt <alexanderschmidt1@gmail.com>
2015-12-03 18:10:37 +0100

    Add names

--- app/controllers/view1.js
+++ app/controllers/view1.js
@@ -7,5 +7,6 @@ var controllers = angular.module('controllers');
 controllers.controller('View1Controller', [ '$scope',
     function ($scope) {
         $scope.viewName = 'view1';
+        $scope.names = [ 'Jane Doe', 'John Doe', 'Jane Smith', 'Pete' ];
     }
 ]);
